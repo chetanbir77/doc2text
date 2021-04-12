96abd8b4fb8221e815d1cc15493cbb8f18ce930d
chetanbir77 <chetanbir.s@felixhealthcare.ai>
2021-04-12 17:13:23 +0530

    updated code

--- doc2text/__init__.py
+++ doc2text/__init__.py
@@ -1,16 +1,22 @@
 # coding=utf-8
 
+# coding=utf-8
+
 import os
 import mimetypes
 
 import PyPDF2 as pyPdf
 import PythonMagick
 import cv2
+import img2pdf
+import numpy
+import pdf2image
+from pdf2image import convert_from_path
 
 from .page import Page
 
 
-acceptable_mime = ["image/bmp", "image/png", "image/tiff", "image/jpeg",
+acceptable_mime = ["image/bmp", "image/png",  "image/jpeg",
                    "image/jpg", "video/JPEG", "video/jpeg2000"]
 
 
@@ -29,6 +35,7 @@ class Document(object):
         self.error = None
 
     def read(self, path):
+
         self.filename = os.path.basename(path)
         self.file_basename, self.file_extension = os.path.splitext(self.filename)
         self.path = path
@@ -39,32 +46,50 @@ class Document(object):
         if self.mime_type[0] == "application/pdf":
             file_temp = open(self.path, 'rb')
             pdf_reader = pyPdf.PdfFileReader(file_temp)
+
+
             self.num_pages = pdf_reader.numPages
             try:
-                for i in xrange(self.num_pages):
+                #for i in range(self.num_pages):
+
+
+                    # im = PythonMagick.Image()
+                    # im.density("300")
+                    # im.read(path)
+                    # im.write(im_path)
+
+                images = convert_from_path(self.path)
+                i=0
+                for image in images:
                     output = pyPdf.PdfFileWriter()
                     output.addPage(pdf_reader.getPage(i))
                     path = 'temp.pdf'
                     im_path = 'temp.png'
                     with open(path, 'wb') as f:
                         output.write(f)
-                    im = PythonMagick.Image()
-                    im.density("300")
-                    im.read(path)
-                    im.write(im_path)
+                    #orig_im = cv2.imread(im_path, 0)
+
+                    # Convert RGB to BGR
+                    image = image.save(im_path)
+                    i=i+1
+
                     orig_im = cv2.imread(im_path, 0)
                     page = Page(orig_im, i, self.lang)
                     self.pages.append(page)
-                    os.remove(path)
-                    os.remove(im_path)
+                        # os.remove(path)
+                        # os.remove(im_path)
                 self.prepared = True
             except Exception as e:
                 self.error = e
                 raise
+        elif self.mime_type[0]=="image/tiff" or self.mime_type[0]=="image/tif" or self.mime_type[0]=="image/TIFF":
+            pass
 
         # If the file is an image, think of it as a 1-page pdf.
         elif self.mime_type[0] in acceptable_mime:
             self.num_pages = 1
+
+
             im = PythonMagick.Image()
             im.density("300")
             im.read(path)
@@ -106,4 +131,5 @@ class Document(object):
 
     def save_pages(self):
         # TODO
+
         stuff = stuff
